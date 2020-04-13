25e0c4cc6700dc09378000093b50ab47c464e1fe
Mark Cascardi <mcascardi@gmail.com>
2020-04-13 03:17:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,9 +7,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  index = input.to_i - 1
-end
+# def input_to_index(input)
+#   index = input.to_i - 1
+# end
 
 def move(board, index, player = "X")
   board[index] = player
